a46a7ab5066fae2c13677bd3862b0e9692c0199b
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-11-06 17:24:03 -0600

    review of DOM

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+const btn = document.getElementById('btn')
+
+const clickFunction = () => {
+    console.log('Hello world')
+    // add an element to the DOM
+    const div = document.createElement('div')
+    div.textContent = 'our team is the best'
+    document.body.appendChild(div)
+}
+
+btn.addEventListener("click", clickFunction)
+
